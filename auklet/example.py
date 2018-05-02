4a74e270068ad63e83d89d45950e0388a8c834b7
Tyler Wiersing <tyler.wiersing@gmail.com>
2018-05-02 15:27:20 -0400

    removing some test stuff

--- auklet/example.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['new_func']
-
-
-def new_func():
-    from auklet.example_app import g
-    for item1 in [1, 2, 3, 4, 5, 6, 7, 8]:
-        for item2 in [1, 2, 3, 4, 5, 6, 7, 8]:
-            g()
-    return
