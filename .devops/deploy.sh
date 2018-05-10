14d2e34fc772cbfc72871760289ffceb637faaf4
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-05-10 09:48:31 -0400

    Fix register command

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -16,8 +16,12 @@ echo 'Deploying to PyPI...'
 sudo pip install -U setuptools twine wheel
 # Make and upload the distribution.
 python setup.py sdist bdist_wheel
-twine register $(cd dist ; ls *.gz)
-twine upload dist/*
+if [[ "$TWINE_REPOSITORY_URL" != "" ]]; then
+  twine register --repository-url $TWINE_REPOSITORY_URL $(cd dist ; ls *.gz)
+  twine upload --repository-url $TWINE_REPOSITORY_URL dist/*
+else
+  twine upload dist/*
+fi
 if [[ "$ENVDIR" == "production" ]]; then
   # Push to public GitHub repo.
   git remote add aukletio git@github.com:aukletio/Auklet-Agent-Python.git
