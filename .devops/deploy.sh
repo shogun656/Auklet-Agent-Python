76ef2614777095364f0376e0702529b33da590f5
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-05-10 09:46:14 -0400

    Clean up register command

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -9,16 +9,16 @@ VERSION="$(cat ~/.version)"
 VERSION_SIMPLE=$(cat ~/.version | xargs | cut -f1 -d"+")
 VERSION_PEP440=$(cat ~/.version440)
 export TIMESTAMP="$(date --rfc-3339=seconds | sed 's/ /T/')"
+
+echo 'Deploying to PyPI...'
+# Update setuptools so we have a version that supports Markdown READMEs.
+# twine is also required.
+sudo pip install -U setuptools twine wheel
+# Make and upload the distribution.
+python setup.py sdist bdist_wheel
+twine register $(cd dist ; ls *.gz)
+twine upload dist/*
 if [[ "$ENVDIR" == "production" ]]; then
-  echo 'Deploying to PyPI...'
-  # Update setuptools so we have a version that supports Markdown READMEs.
-  # twine is also required.
-  sudo pip install -U setuptools twine wheel
-  # Make and upload the distribution.
-  python setup.py sdist bdist_wheel
-  ls -al dist
-  #twine register --repository-url https://test.pypi.org/legacy/ ???
-  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
   # Push to public GitHub repo.
   git remote add aukletio git@github.com:aukletio/Auklet-Agent-Python.git
   git push aukletio HEAD:master
