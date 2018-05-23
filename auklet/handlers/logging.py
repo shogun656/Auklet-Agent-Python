8bdd3126884b8b6a6f632b5791802b9ea72499fb
Tyler Wiersing <tyler.wiersing@gmail.com>
2018-05-23 13:23:15 -0400

    pep8

--- auklet/handlers/logging.py
+++ auklet/handlers/logging.py
@@ -14,7 +14,8 @@ class AukletHandler(logging.Handler):
     def __init__(self, apikey=None, app_id=None,
                  base_url="https://api.auklet.io/", *args, **kwargs):
         self.client = Client(apikey, app_id, base_url, get_mac())
-        logging.Handler.__init__(self, level=kwargs.get('level', logging.NOTSET))
+        logging.Handler.__init__(self,
+                                 level=kwargs.get('level', logging.NOTSET))
 
     def emit(self, record):
         self.format(record)
