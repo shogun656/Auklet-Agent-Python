1e1cf9f711145878dd301ca0f380e47e68e26702
joshuahufford <joshuamh1135@gmail.com>
2018-06-19 11:27:00 -0400

    Continue to make code more readable.

--- src/benchmark/base.py
+++ src/benchmark/base.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import os
 import itertools
 from src.benchmark.statprof import statprof
 from pidigits import piGenerator
@@ -8,7 +7,7 @@ __all__ = ['start']
 
 
 class ThreadRing:
-    def test(self, loop_counter):
+    def test(self, loop_counter, state):
         statprof.start()
         try:
             print("Starting Thread Ring tests...")
@@ -18,10 +17,10 @@ class ThreadRing:
         finally:
             statprof.stop()
             print("Thread Ring test results...")
-            self.display()
+            display(state, __class__.__name__)
 
     @staticmethod
-    def generator(n=50000, n_threads=400, cycle=itertools.cycle):
+    def generator(n=50000, n_threads=400, cycle=itertools.cycle):     # 50000, 400
         def worker(worker_id):
 
             n = 1
@@ -40,15 +39,9 @@ class ThreadRing:
             except StopIteration:
                 break
 
-    def display(self):
-        with open("tmp/benchmark_results", 'a') as file:
-            file.write(os.path.basename(__file__) + " " + self.__class__.__name__ + " ")
-        statprof.display()
-        statprof.reset()
-
 
 class Fibonacci:
-    def test(self, loop_counter, fibonacci_range=20):
+    def test(self, loop_counter, state, fibonacci_range=20):        # 20
         statprof.start()
         try:
             print("\nStarting Fibonacci Sequence tests...")
@@ -58,7 +51,7 @@ class Fibonacci:
         finally:
             statprof.stop()
             print("Fibonacci sequence test results...")
-            self.display()
+            display(state, __class__.__name__)
 
     def generator(self, fibonacci_range):
         if fibonacci_range <= 1:
@@ -66,15 +59,9 @@ class Fibonacci:
         else:
             return self.generator(fibonacci_range-1) + self.generator(fibonacci_range-2)
 
-    def display(self):
-        with open("tmp/benchmark_results", 'a') as file:
-            file.write(os.path.basename(__file__) + " " + self.__class__.__name__ + " ")
-        statprof.display()
-        statprof.reset()
-
 
 class Pi:
-    def test(self, loop_counter, number_of_pi_digits=1000):
+    def test(self, loop_counter, state, number_of_pi_digits=1000):      # 1000
         statprof.start()
         try:
             print("\nStarting Pi Generator tests...")
@@ -84,22 +71,23 @@ class Pi:
         finally:
             statprof.stop()
             print("Pi Generator test results...")
-            self.display()
+            display(state, __class__.__name__)
 
     @staticmethod
     def generator(number_of_digits):
         my_pi = piGenerator()
         return [next(my_pi) for _ in range(number_of_digits)]
 
-    def display(self):
-        with open("tmp/benchmark_results", 'a') as file:
-            file.write(os.path.basename(__file__) + " " + self.__class__.__name__ + " ")
-        statprof.display()
-        statprof.reset()
+
+def display(state, class_name):
+    with open("tmp/benchmark_results", 'a') as file:
+        file.write(state + " " + class_name + " ")
+    statprof.display()
+    statprof.reset()
 
 
-def start():
+def start(state):
     loop_counter = 10
-    ThreadRing().test(loop_counter)
-    Fibonacci().test(loop_counter)
-    Pi().test(loop_counter)
+    ThreadRing().test(loop_counter, state)
+    Fibonacci().test(loop_counter, state)
+    Pi().test(loop_counter, state)
--- src/benchmark/run_tests.py
+++ src/benchmark/run_tests.py
@@ -4,9 +4,14 @@ from auklet.monitoring import Monitoring
 from src.benchmark import base
 
 
+def without_auklet():
+    print("\n\nStarting benchmark tests without the Auklet Agent...")
+    base.start(state="WithoutAuklet")
+
+
 @patch('auklet.base.Client.update_limits')
 @patch('auklet.base.Client._get_kafka_certs')
-def auklet_benchmark(get_kafka_certs_mock, update_limits_mock):
+def with_auklet(get_kafka_certs_mock, update_limits_mock):
     def _get_kafka_brokers(self):
         self.brokers = ["kafka:9093"]
         self.producer_types = {
@@ -21,14 +26,9 @@ def auklet_benchmark(get_kafka_certs_mock, update_limits_mock):
     patcher.start()
     auklet_monitoring = Monitoring("", "", monitoring=True)
     auklet_monitoring.start()
-    base.start()
+    base.start(state="WithAuklet")
     auklet_monitoring.stop()
     patcher.stop()
-
-
-def control_benchmark():
-    print("\n\nStarting benchmark tests without the Auklet Agent...")
-    base.start()
 
 
 def display_complete_results():
@@ -37,7 +37,7 @@ def display_complete_results():
         my_list = tuple(tuple(map(str, line.split())) for line in file)
 
     try:
-        print("\n\nTest comparison for run time only of " + my_list[0][0] + " and " + my_list[3][0])
+        print("\n\nTest comparison for run time only for with the Auklet Agent versus without the Auklet Agent")
         p2(my_list[0][0].split('_')[0], my_list[3][0].split('_')[0])
         p3("seconds", "seconds", "name")
         for i in range(0, 3):
@@ -57,12 +57,12 @@ def p2(column1, column2):
 
 
 def p3(column_1, column_2, column_3):
-    print(column_1, column_2, column_3, sep='\t')
+    print(column_1, column_2, column_3, sep='\t\t')
 
 
 def main():
-    control_benchmark()
-    auklet_benchmark()
+    without_auklet()
+    with_auklet()
     display_complete_results()
 
 
