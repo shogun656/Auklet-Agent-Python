120646973858eeb48748c2c2dc97df8eca826b3d
joshuahufford <joshuamh1135@gmail.com>
2018-07-09 15:10:02 -0400

    Fixes test runner

--- unit_test.sh
+++ unit_test.sh
@@ -11,9 +11,11 @@ pyenv global 3.6.3
 
 pip3 install mock kafka
 
+python3 setup.py install
+
 for file in "tests/monitoring/test___init__.py" "tests/monitoring/test_logging.py" "tests/monitoring/test_sampling.py" "tests/test_base.py" "tests/test_errors.py" "tests/test_stats.py"
 do
-        python3 $file
+    python3 $file
 done
 
 rm -R .auklet
