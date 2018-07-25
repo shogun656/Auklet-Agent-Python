01f4c6423236fe1662b7932de6c8437ca1de5b16
joshuahufford <joshuamh1135@gmail.com>
2018-07-25 17:30:57 -0400

    Change test run organization

--- .devops/test_and_coverage.sh
+++ .devops/test_and_coverage.sh
@@ -15,10 +15,10 @@ python setup.py install
 
 pip install tox tox-pyenv
 pip install --upgrade setuptools
+pyenv install 3.6.3
+pyenv local 2.7.12 3.6.3
 
 if [[ "$CIRCLE_LOCAL_BUILD" == 'false' ]]; then
-  pyenv install 3.6.3
-  pyenv local 2.7.12 3.6.3
   curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
   chmod +x ./cc-test-reporter
   ./cc-test-reporter before-build
