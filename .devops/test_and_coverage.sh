518deb9cf09b3029c0951074712208c4eecf47ff
joshuahufford <joshuamh1135@gmail.com>
2018-07-25 17:37:50 -0400

    Install coverage

--- .devops/test_and_coverage.sh
+++ .devops/test_and_coverage.sh
@@ -13,7 +13,7 @@ CIRCLE_LOCAL_BUILD=$1
 
 python setup.py install
 
-pip install tox tox-pyenv
+pip install coverage tox tox-pyenv
 pip install --upgrade setuptools
 pyenv install 3.6.3
 pyenv local 2.7.12 3.6.3
