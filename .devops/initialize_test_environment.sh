6f43eae1caf46b4782f98a89f793a4e10525782c
joshuahufford <joshuamh1135@gmail.com>
2018-07-30 12:46:39 -0400

    installs exact version if passed

--- .devops/initialize_test_environment.sh
+++ .devops/initialize_test_environment.sh
@@ -4,16 +4,19 @@ set -e
 
 VERSION=$1
 
-if [ $VERSION == '3.7.0' ]; then
-    INSTALL_LATEST='false'
+if [ "${#VERSION}" -gt 4 ]; then  # This checks if a complete version is passed
+    INSTALL_LATEST='false'      # and if so, it will install that exact version
+else
+    INSTALL_LATEST='true'
 fi
 
 eval "$(pyenv init -)"
 
+echo $INSTALL_LATEST
+
 if [[ "$INSTALL_LATEST" == 'false' ]]; then
     pyenv install $VERSION
     LATEST_VERSION=$(pyenv versions | grep $VERSION | grep -v '2.7.12' | grep -v '3.5.2' | grep -v system)
-
     pyenv global $LATEST_VERSION
 else
     pyenv install-latest $VERSION
