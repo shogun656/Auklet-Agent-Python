a7de5c4dcec468840730602b364e34a4828264d8
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-11-13 16:54:12 -0500

    Drop unnecessary test commands

--- .devops/tests.sh
+++ .devops/tests.sh
@@ -13,9 +13,6 @@ done
 touch key.pem
 zip key.pem.zip key.pem
 
-#pip install coverage
-#python setup.py install
-
 # This outputs the complete current python version to `pyver`
 pyver=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
 echo Python $pyver
